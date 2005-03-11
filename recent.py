00c7cb50084a1489b237ada4b9a455c99daf6512
nguyen tien hai <thnguyen@dental-on-line.fr>
2005-03-11 00:00:00 +0000

    Version 1.3
    no add cvs adn svn temp file
    format edited file list better way

--- recent.py
+++ recent.py
@@ -7,28 +7,45 @@ import time
 def usage():
     print "usage : recent.py [filename]"
 
+def addToRecentFile(filename):
+    tmpfilename=os.path.join(os.environ["HOME"],'.vim/.recent_files.tmp')
+    recent_files= os.path.join(os.environ["HOME"],'.vim/recent_files')
+    date=time.strftime('%Y%m%d %H:%M' ,time.localtime())
+    filename=filename.replace(os.environ["HOME"],"~")
+    
+    tmpfile=open(tmpfilename, "a")
+
+    if os.path.exists(recent_files):
+        fic=open(recent_files, 'r')
+        for line in fic.readlines():
+            if line.split(" ")[0] != filename:
+                print >> tmpfile, line[:-1]
+
+        fic.close()
+
+    print >> tmpfile,filename.ljust(49) + " " + date
+    tmpfile.close()
+
+    os.rename(tmpfilename, recent_files)
+
+
+### MAIN ####
+
 if len(sys.argv) == 1:
     usage()
     sys.exit(1)
 
-### MAIN ####
+absfilename=sys.argv[1]
+filename=absfilename.split('/')[-1]
+append=True
 
-filename=sys.argv[1]
-tmpfilename=os.path.join(os.environ["HOME"],'.vim/.recent_files.tmp')
-recent_files= os.path.join(os.environ["HOME"],'.vim/recent_files')
-date=time.strftime('%Y%m%d %H:%M' ,time.localtime())
+# Not append svn temp file
+if filename[:3] == 'svn':
+    append=False
 
-tmpfile=open(tmpfilename, "a")
+# Not append cvs temp file
+if filename[:3] == 'cvs':
+    append=False
 
-if os.path.exists(recent_files):
-    fic=open(recent_files, 'r')
-    for line in fic.readlines():
-        if line[:-19] != filename:
-            print >> tmpfile, line[:-1]
-
-    fic.close()
-
-print >> tmpfile,filename + "    " + date
-tmpfile.close()
-
-os.rename(tmpfilename, recent_files)
+if append:
+    addToRecentFile(absfilename)
